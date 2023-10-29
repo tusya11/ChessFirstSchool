983d30596c7e3c5e6f942f58c2972306924ce736
Natalia Sereda <asereda.exceedteam@gmail.com>
2023-10-29 11:24:21 +0300

    feat: add offer & privacy policy

--- src/parts/Footer/Footer.jsx
+++ src/parts/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import SocialLink from "../../components/SocialLink/SocialLink";
 import { FaWhatsapp } from "react-icons/fa";
 import { SlSocialVkontakte } from "react-icons/sl";
+import offer from "../../docs/offer.pdf";
+import privacyPolicy from "../../docs/privacy_policy.pdf";
 import "./Footer.scss";
 
 const Footer = () => (
@@ -10,7 +12,14 @@ const Footer = () => (
         src="https://thumb.tildacdn.com/tild3139-3438-4465-b834-656330343861/-/resize/240x/-/format/webp/1.png"
         alt="logo"
       />
-      <span>Политика конфиденциальности</span>
+      <div className="footer__docs">
+        <a href={offer} target="_blank" rel="noreferrer">
+          Договор оферты
+        </a>
+        <a href={privacyPolicy} target="_blank" rel="noreferrer">
+          Политика конфиденциальности
+        </a>
+      </div>
       <div className="footer__icons">
         <SocialLink icon={<SlSocialVkontakte />} />
         <SocialLink icon={<FaWhatsapp />} />
