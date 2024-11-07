f61670f7b91f6ed1d13935d4f1022025ab7d3b14
Natalia Sereda <asereda.exceedteam@gmail.com>
2024-11-07 21:41:29 +0300

    feat: add notification on google mail

--- src/components/ModalContent/SignUpContent/consts.js
+++ src/components/ModalContent/SignUpContent/consts.js
@@ -1,2 +1,2 @@
 export const scriptURL =
-  "https://script.google.com/macros/s/AKfycbw7JckR2Ovh4g-oSKkI5DCNTbDSxhN_cJJ-GTMnmXKI8Uo-GUtcJcxcIXavIZS8MR8/exec";
+  "https://script.google.com/macros/s/AKfycbwm6UOwMqvArzOfpjQ4rwy8buMzwOPC2VHcZDSDaXbdSTz2uOlMBHFJcp6zdLXXtc7s/exec";
