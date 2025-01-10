6e253d3df75b4214c6eab6f84857f3e77588d129
Natalia Sereda <asereda.exceedteam@gmail.com>
2025-01-11 02:38:02 +0300

    feat: disable page with old prices

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import SuccessPage from "./pages/SuccessPage/SuccessPage";
 import FailurePage from "./pages/FailurePage/FailurePage";
 import Wrapper from "./Wrapper";
-import NewPrices from "./NewPrices";
+// import NewPrices from "./NewPrices";
 import "./App.css";
 
 const App = () => (
@@ -10,10 +10,10 @@ const App = () => (
     <BrowserRouter>
       <Routes>
         <Route path="/main/*" element={<Wrapper />} />
-        <Route path="/old-prices/*" element={<NewPrices />} />
+        {/* <Route path="/old-prices/*" element={<NewPrices />} /> */}
         <Route path="/success/*" element={<SuccessPage />} />
         <Route path="/fail/*" element={<FailurePage />} />
-        <Route path="/" element={<Navigate to="/main" replace />} />
+        <Route path="/*" element={<Navigate to="/main" replace />} />
       </Routes>
     </BrowserRouter>
   </div>
