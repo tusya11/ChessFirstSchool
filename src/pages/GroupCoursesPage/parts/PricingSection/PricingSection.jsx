aa42fdb4e46cdfd33322bab8989d575650fbdbe3
tusya11 <seredanatali250201@gmail.com>
2025-06-07 12:12:24 +0300

    fix: update date  & delete block

--- src/pages/GroupCoursesPage/parts/PricingSection/PricingSection.jsx
+++ src/pages/GroupCoursesPage/parts/PricingSection/PricingSection.jsx
@@ -44,12 +44,6 @@ const PricingSection = ({ price }) => {
               <svg className="feature-icon" viewBox="0 0 24 24">
                 <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/>
               </svg>
-              Входное тестирование
-            </li>
-            <li className="feature-item">
-              <svg className="feature-icon" viewBox="0 0 24 24">
-                <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/>
-              </svg>
               Составление индивидуальной программы
             </li>
             <li className="feature-item">
